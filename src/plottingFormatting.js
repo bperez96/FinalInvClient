2f26a919161a63d702385e8402b0623b45d45f18
bperez96 <bernardoperez@utexas.edu>
2022-02-17 15:59:33 -0600

    changing display on value graph

--- src/plottingFormatting.js
+++ src/plottingFormatting.js
@@ -315,7 +315,7 @@ function returnComparissonGraph(  pData ){
             domain={[{minRel},{maxRel}]}
             type = "number"
             tickFormatter={regularRound}
-            label={{value: 'Normalized Value', angle: -90, fill : 'white', fontSize: '1.75vw', dx: 5 , font: 'Arial'}}
+            label={{value: 'Normalized Value', angle: -90, fill : 'white', fontSize: '1.75vw', dx: "20%" , font: 'Arial'}}
           />
           <Tooltip content={<CustomComparissonTooltip />}  />
           <Legend />
