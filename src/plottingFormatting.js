0811ca6e1ce027948af4a68a15c4aa814864bea1
bperez96 <bernardoperez@utexas.edu>
2022-02-16 21:59:51 -0600

    adding log on tab

--- src/plottingFormatting.js
+++ src/plottingFormatting.js
@@ -246,7 +246,7 @@ function returnSpendGraph(  pData ){
             domain={[{spendMin},{spendMax}]}
             type = "number"
             tickFormatter={priceFormatter}
-            label={{value: 'Value', angle: -90, fill : 'white', fontSize: '1.75vw',dx:{5}, font: 'Arial'}}
+            label={{value: 'Value', angle: -90, fill : 'white', fontSize: '1.75vw',dx: 5, font: 'Arial'}}
           />
           <Tooltip content={<CustomProfitTooltip />}  />
           <Legend content = {getNothing}/>
